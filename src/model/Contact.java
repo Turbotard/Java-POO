1d26e7eb5cbbfa0909032ce0db53265e0d5b2872
Turbotard <turbotard9@gmail.com>
2023-01-09 15:11:45 +0100

    petite modif

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -1,16 +1,20 @@
 package model;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Contact {
-    public String number;
-    public String prenom;
-    public String nom;
-    static ArrayList<Contact> contacteListe = new ArrayList<Contact>();
+    private String _number;
+    private String _prenom;
+    private String _nom;
+    private String _mail;
+    private Date _date;
+    private static ArrayList<Contact> contacteListe = new ArrayList<Contact>();
 
-    public Contact(String number_, String prenom_, String nom_) {
-        number = number_;
-        prenom = prenom_;
-        nom = nom_;
+    public Contact(String number, String prenom, String nom) {
+        _number = number;
+        _prenom = prenom;
+        _nom = nom;
     }
 
     public static void ajouterContact(Contact contact) {
@@ -23,8 +27,8 @@ public class Contact {
 
     public static void afficherContact(String prenom_, String nom_) {
         for (Contact contact : contacteListe) {
-            if (contact.nom.equals(nom_) || contact.nom.equals(prenom_)) {
-                System.out.println(contact.nom + " " + contact.prenom + " " + contact.number);
+            if (contact._nom.equals(nom_) || contact._nom.equals(prenom_)) {
+                System.out.println(contact._nom + " " + contact._prenom + " " + contact._number);
                 break;
             } else {
                 System.out.println("Pas trouvé apprend à écrire ou recherche un contact qui existe !");

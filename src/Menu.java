fe671bc8c0460cea535aa07c4d1f815cae9853af
justoverfr <justover.fr@gmail.com>
2023-01-09 12:08:58 +0100

    youpi ça marche

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void displayMenu() {
         System.out.println("1. Ajouter un contact");
         System.out.println("2. Afficher les contacts");
         System.out.println("q. Quitter le menu");
+
+        String input = getUserInput();
+
+        while (!input.equals("1") && !input.equals("2") && !input.equals("q")) {
+            System.out.println("Veuillez entrer une option valide");
+            input = getUserInput();
+        }
+
+        if (input.equals("1")) {
+            addContact();
+        } else if (input.equals("2")) {
+            displayContacts();
+        } else if (input.equals("q")) {
+            quit();
+        }
+    }
+
+    public static String getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez une option :");
+        String userInput = scanner.nextLine();
+
+        System.out.println("Input : " + userInput + "\n");
+        scanner.close();
+        return userInput;
+    }
+
+    public static void addContact() {
+        System.out.println("Contact ajouté");
+    }
+
+    public static void displayContacts() {
+        System.out.println("Contacts affichés");
+    }
+
+    public static void quit() {
+        System.out.println("Au revoir");
     }
 }

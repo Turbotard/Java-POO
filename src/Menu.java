c88aa8d9f698d1874dfa7b284927c9dc95f26683
justoverfr <justover.fr@gmail.com>
2023-01-09 11:31:40 +0100

    premier menu best of

---
+++ src/Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+    public static void displayMenu() {
+        System.out.println("1. Ajouter un contact");
+        System.out.println("2. Afficher les contacts");
+        System.out.println("q. Quitter le menu");
+    }
+}

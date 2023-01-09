e9a972ccd9e6f2fce5f138987df0b7f9412546fc
Turbotard <turbotard9@gmail.com>
2023-01-09 12:25:51 +0100

    Ajouter contact

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Menu.displayMenu();
-        Contact.contact();
     }
 }
--- src/Contact.java
+++ src/Contact.java
@@ -1,14 +1,19 @@
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Contact {
-    public static void contact(){
-        int i ;
-        Map<String, String> i = new HashMap<>(); 
-        i.put("Numéro", "0");
-        i.put("Nom", "Le nom");
-        i.put("rénom", "Prénom");
-        i.add(new String("Autre nom"));
-        i.get("Nom");
+    String number;
+    String prenom;
+    String nom;
+    ArrayList<Contact> contacteListe = new ArrayList<Contact>();
+
+    public void Contact(String number_, String prenom_, String nom_){
+        number = number_;
+        prenom = prenom_;
+        nom = nom_;
+        contacteListe.add(this);
+        
     }
 }
